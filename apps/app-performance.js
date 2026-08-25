365c893f3bf653343ea096f5ca2980bb6961a6db
0pticbox <0pticboxsound@gmail.com>
2026-08-25 15:57:54 -0500

    Load extra Distortion strobe keys

--- apps/app-performance.js
+++ apps/app-performance.js
@@ -215,6 +215,7 @@
 
     load('./strobe-fx.js?v=20260824-3', 'data-distortion-strobe-pack');
     load('./clip-trim.js?v=20260825-1', 'data-distortion-clip-trim');
+    load('./extra-strobes.js?v=20260825-1', 'data-distortion-extra-strobes');
   };
 
   if (document.readyState === 'loading') {
